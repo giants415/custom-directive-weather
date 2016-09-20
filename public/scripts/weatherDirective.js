3cfb8d3ef6271fc213ff5b3bf97da79400413157
Andrew <andrew.vinocur@gmail.com>
2016-09-20 13:33:46 -0700

    changes from inline to broken down controller

--- public/scripts/weatherDirective.js
+++ public/scripts/weatherDirective.js
@@ -4,27 +4,34 @@ angular.module('weatherApp')
   function currentWeather () {
     var directive = {
       scope: {
-        city: '@'
+        city: '@',
+        weather: '@'
       },
       restrict: 'EA',
       templateUrl: '/views/templates/weatherDirective.html',
       replace: true,
       controllerAs: 'weatherCardCtrl',
-      controller: ['$http', function($http){ var vm = this; var url="http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q="; var apikey = "&appid=" + 'feabb831e3da04d66be31f4a618123ef'
-        ; vm.getWeather = function(city){ $http({method: 'JSONP', url: url + city + apikey}) .success(function(data){ vm.weather = data; }); } }],
+      controller: WeatherCardController()
     };
     return directive;
   }
 
-  // function WeatherCardController($http) {
-  //    controller: ['$http', function($http){
-  //      var vm = this;
-  //      var url="http://api.openweathermap.org/data/2.5/weather?q=";
-  //      var apikey = "&appid=" + 'feabb831e3da04d66be31f4a618123ef';
-  //      vm.getWeather = function(city){
-  //        $http({
-  //          method: 'JSONP',
-  //          url: url + city + apikey
-  //         }).success(function(data){ vm.weather = data; });
-  //        }
-  //       }]
+  function WeatherCardController($http) {
+     controller: ['$http', function($http){
+       var vm = this;
+       var url="http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q=";
+       var apikey = "&appid=" + 'feabb831e3da04d66be31f4a618123ef';
+       vm.getWeather = function(city){
+         $http({
+           method: 'JSONP',
+           url: url + city + apikey
+          }).success(function(data){
+            vm.weather = data;
+            console.log(data);
+            });
+         }
+        }]
+  }
+
+  // ['$http', function($http){ var vm = this; var url="http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q="; var apikey = "&appid=" + 'feabb831e3da04d66be31f4a618123ef'
+  //   ; vm.getWeather = function(city){ $http({method: 'JSONP', url: url + city + apikey}) .success(function(data){ vm.weather = data; }); } }],
